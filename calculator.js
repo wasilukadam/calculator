171ca6149227f7f9990b5f4cace6246b3638e144
Marcin Wasiluk <marcin@wasiluk.com>
2024-11-24 19:56:45 +0000

    Adam calculator WIP

---
+++ calculator.js
@@ -0,0 +1,21 @@
+var box=1;
+function adamsFunctionToDisplaySomething(){
+    alert('someone pressed the button :-)')
+}
+
+
+function adampressedabutton(parameterA) {
+alert('someone pressed '.concat(parameterA))
+}
+
+    function myFunction() {
+      box = box+1;
+      document.getElementById("myText").innerHTML = box;
+    }
+    function cancelCalculator() {
+    box=-1;
+
+    }
+function addingup(number1){
+box=box+number1;
+}
